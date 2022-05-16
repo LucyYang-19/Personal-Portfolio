50dec07f685cd2a28768d4274dc15c6d23b56ac5
LucyYang-19 <93957029+LucyYang-19@users.noreply.github.com>
2022-05-15 18:32:52 -0700

    pages

--- src/front-end/script.js
+++ src/front-end/script.js
@@ -1,15 +1,17 @@
 // Scroll to specific values
 // scrollTo is the same
 window.scroll({
-    top: 2500, 
-    left: 0, 
-    behavior: 'smooth'
-  });
-  
-  // Scroll certain amounts from current position 
-  window.scrollBy({ 
-    top: 100, // could be negative value
-    left: 0, 
-    behavior: 'smooth' 
-  });
-  
+  top: 2500,
+  left: 0,
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: 'smooth'
+});
+
+// image caro
+
